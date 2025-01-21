72ca24a6209775ed34a98d2d2ec775ce15864a90
Mauro Montano <mauro.montano6789@gmail.com>
2025-01-21 11:20:55 -0300

    init

---
+++ app/javascript/controllers/modal_controller.js
@@ -0,0 +1,21 @@
+import { Controller } from "stimulus";
+
+export default class extends Controller {
+  static targets = ["modal"];
+
+  // Abrir el modal según el id
+  open(event) {
+    const modalId = event.target.dataset.modalTarget;
+    const modal = document.getElementById(modalId);
+
+    if (modal) {
+      modal.classList.remove("hidden");
+    }
+  }
+
+  // Cerrar cualquier modal
+  close(event) {
+    const modal = event.target.closest(".modal");
+    modal.classList.add("hidden");
+  }
+}
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get "home/index"
+  # root "movies#index"
+
+  # get "movies", to: "movies#index", as: "movies"
+  # get "movies/:id", to: "movies#show", as: "movie"
+end
