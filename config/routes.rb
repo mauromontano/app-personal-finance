20f6be107b76e1519410d32a94e9e7174ee49724
Mauro Montano <mauro.montano6789@gmail.com>
2025-02-07 13:05:05 -0300

    Login flow phase 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  get "home/index"
+  get "public/index"
   devise_for :users
 
-  # Routes
-  root "home#index"
-  get "home", to: "home#index", as: "home"
+  authenticated :user do
+    root to: "home#index", as: :authenticated_root
+  end
+
+  unauthenticated do
+    root to: "public#index", as: :unauthenticated_root
+  end
 end
